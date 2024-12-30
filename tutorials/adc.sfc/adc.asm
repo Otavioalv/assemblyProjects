0818112e3dacb4a686b1667d8e49af93087d23af
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2024-12-30 18:32:01 -0400

    alegir

--- tutorials/adc.sfc/adc.asm
+++ tutorials/adc.sfc/adc.asm
@@ -15,3 +15,54 @@ seek($8000)
     nop                     // "no operation"
     rep #$30                // Configura o registradoa A, X e Y para 16 bits
     
+    // adc
+    lda #$1000              // Tranafere o valor para A
+    clc                     // limpa flag C
+    adc #$1234              // soma o valor com A
+
+    // memoria/sbc
+    tax                     // Transfere o valor de A para X
+    lda #$1000              // Transfere o valor para A
+    sta $0000               // Salva o valor de a na memoria especificada
+    txa                     // Transfere o valor de X para A
+    sec                     // Seta o valor 1 na flag C
+    sbc $0000               // Subtrai o A em em (16 bits) que e C com o valor especificado
+
+    // inc
+    inc                     // incrementa A em (16 bits) que e C
+    inc $0000               // incrementa o valor na posição na memoria especificada
+
+    // dec
+    dec                     // Decrementa o A em (16 bits) que e C
+    dec $0000               // Decrementa o valor na posição da memoria especificada
+
+    // X e Y (não tem add e sbc pro X e Y)
+    inx                     // Incrementar X
+    iny                     // Incrementar Y
+    dex                     // Decrementar X
+    dey                     // Decrementar Y
+
+    // Carry do adc
+    lda #$0001              // Transfere o valor para A
+    sec                     // Seta  C
+    adc #$0001              // Realiza soma com o valor especificado + A
+
+    // Carry do sbc
+    lda #$0005              // Transfere o valor para A
+    clc                     // limpa a flag C
+    sbc #$0001              // Subtrai A como o valor especificado
+
+    // Atuação nas flags
+    lda #$7fff              // Transfere o valor para A
+    inc                     // Soma 1 com A em 16 bits
+
+    lda #$ffff              // Transfe o valor para A
+    inc                     // Soma 1 com A em 16 bits
+
+    lda #$1000              // Tranafere o valor para A
+    sec                     // Seta C
+    sbc #$1000              // Subtrai A em 16 bits com o valor especificado
+
+-   
+    bra -
+
